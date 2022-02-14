79510b6afdf6beea185249617db8b73e2a5ac9f2
preciousnwaoha <nwaohaprecious25@gmail.com>
2022-02-14 03:29:55 +0100

    Restructure and add day
    Added Days Folders and Completed Day 8

--- Day_7/day_7_Challenge_5_hangman.py
+++ Day_7/day_7_Challenge_5_hangman.py
@@ -16,13 +16,16 @@ lives = 6
 from hangman_art import logo
 print(logo)
 
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+# Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
 
 #Create blanks
 display = []
 for _ in range(word_length):
     display += "_"
+
+
+print(f"\nHere's the missing word {' '.join(display)}")
 
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
---
+++ Day_8/day_8.1_Ex_Paint_Cans.py
@@ -0,0 +1,24 @@
+#Write your code below this line 👇
+
+import math
+
+def paint_calc(height, width, cover):
+  area = height * width
+  calc = area/coverage
+  print(int(math.ceil(calc)))
+
+
+
+
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
+
